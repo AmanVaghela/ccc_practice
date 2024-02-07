7de84608121436f766ea46e3245136784c46e779
Aman Vaghela <vaghelaaman523@gmail.com>
2024-02-07 18:30:49 +0530

    MVC

---
+++ mvcpractice/app/Mage.php
@@ -0,0 +1,22 @@
+<?php
+    class Mage
+    {
+        public static function init()
+        {
+            // $request_model_core = new Core_Model_Request();
+            // echo $request_model_core->getRequestUri();
+        //    $Requestmodel= Mage::getModel("core/request"); 
+        //    echo(get_class($Requestmodel));
+        Mage::getModel("core/request"); 
+       
+
+        }
+        public static function getModel($ModelName)
+        {
+            $arr = explode("/", $ModelName);
+           // $uc= array_map("ucfirst",$arr);
+            $dif = ucfirst($arr[0])."_Model_".ucfirst($arr[1]);
+            echo $dif;
+        }
+    }
+?>
